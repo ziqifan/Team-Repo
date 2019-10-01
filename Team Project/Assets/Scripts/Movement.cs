93151009f601f2ce371adc4bf8fe3e6627c5844b
Safa n <safa.nazir@ontariotechu.net>
2019-10-01 09:52:29 -0400

    basic movement + collisions :heart:

---
+++ Team Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 2;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        transform.position += (Vector3.forward * speed) * y * Time.deltaTime;
+        transform.position += (Vector3.right * speed) * x * Time.deltaTime;
+    }
+}
