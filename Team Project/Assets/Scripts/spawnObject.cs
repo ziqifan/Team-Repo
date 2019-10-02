9febe98a25ae0c718bde6456c0b22c1f0c95fe31
Safa n <safa.nazir@ontariotechu.net>
2019-10-02 15:44:22 -0400

    Added jumping and spawning objects

---
+++ Team Project/Assets/Scripts/spawnObject.cs
@@ -0,0 +1,24 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour
+{
+
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this will spawn one thing for each time you press the button, i might switch it to some particle effect idk
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Instantiate(box); 
+        }
+    }
+}
