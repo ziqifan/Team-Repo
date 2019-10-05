da997863f98ee321732af4f1d9fff7e99cf6e9ba
Lillian <ziqi.fan@ontariotech.net>
2019-10-05 17:28:21 -0400

    Update First Person Camera and Movement

---
+++ Team Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float JumpHeight = 20.0f;
+    public bool isGrounded;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(0, JumpHeight, 0);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
